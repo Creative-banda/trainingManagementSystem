b5436a42ebbf7bd5cae93be2d2cd60172d899258
Shoib007 <skhantanda@gmail.com>
2023-11-16 22:36:53 +0530

    Added a new folder in FontEnd named admin-pannal-vite

--- Front-End/eduvate_frontend/app/(home)/layout.js
+++ Front-End/eduvate_frontend/app/(home)/layout.js
@@ -1,22 +1,69 @@
 'use client'
-import AppNavbar from "@/components/navbar";
-import AppSidebar from "@/components/sidebar";
+import React, { useState } from 'react';
+import {
+    AppstoreOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    ShopOutlined,
+    TeamOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, Button } from 'antd';
+const { Header, Content, Sider } = Layout;
 
-export default function DashboardLayout({ children }) {
+const items = [
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    AppstoreOutlined,
+    TeamOutlined,
+    ShopOutlined,
+].map((icon, index) => ({
+    key: String(index + 1),
+    icon: React.createElement(icon),
+    label: `nav ${index + 1}`,
+}));
+
+const HomeLayout = ({ children }) => {
+    const [collapsed, setCollapsed] = useState(true);
+
     return (
-        <main className="flex h-screen w-screen gap-4">
-            <div className="border">
-                <AppSidebar />
-            </div>
+        <Layout hasSider className='h-screen w-screen gap-4'>
+            <Sider trigger={null} collapsible collapsed={collapsed} theme='dark'
+                breakpoint="lg"
+                onBreakpoint={(broken) => {
+                    setCollapsed(!collapsed);
+                }}
+                onCollapse={(collapsed, type) => {
+                    console.log(collapsed, type);
+                }}
+            >
+                <div className="w-full sm:p-8 text-center text-3xl">
+                    {collapsed ? "":'Eduvate'}
+                </div>
+                <Menu mode="inline" defaultSelectedKeys={['4']} items={items} theme='dark' />
+            </Sider>
+            <Layout className='gap-4'>
+                <Header>
+                    <Button
+                        type="text"
+                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                        onClick={() => setCollapsed(!collapsed)}
+                        className='text-white border-white ml-0'
+                    />
+                </Header>
 
-            <div className="flex flex-col gap-4 w-full h-full">
-                <div className="border rounded-md">
-                    <AppNavbar />
-                </div>
-                <div className="">
+                <Content className="p-4 border">
                     {children}
-                </div>
-            </div>
-        </main>
-    )
-}
+                </Content>
+            </Layout>
+        </Layout>
+    );
+};
+export default HomeLayout;
